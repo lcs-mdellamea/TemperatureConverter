6df758cd740c97ce2a355df85912f30e23580fdf
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-02 09:56:26 -0500

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Madison Dellamea on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
