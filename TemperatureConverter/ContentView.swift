02ef4b2120b9d4f388bea9b79b9ea7faa89d0c01
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-02 21:14:09 -0500

    Used string interpolation to create slider and used a property wrapper. Everything on the static layout works great, just need to implement the feedback.

--- TemperatureConverter/ContentView.swift
+++ TemperatureConverter/ContentView.swift
@@ -8,14 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var celsius: Double = -5.00
+
+    var fahrenheit: Double {
+        return (celsius * 9/5) + 32
+    }
+    
+    @State var feedback = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(alignment:.leading){
+            
+            Text("Celsius")
+                .font(.title2)
+                .bold()
+            
+            Slider(value: $celsius,
+                   in: -50.0...50.0,
+                   step: 1.0,
+                   label: {
+                Text("Celsius")
+            },
+                   minimumValueLabel: {
+                Text("-50.0")
+            },
+                   maximumValueLabel: {
+                Text("50.0")
+            })
+
+            HStack {
+                Spacer()
+                Text("\( String(format: "%.0f", celsius)) C°")
+                    .font(.title2)
+                    .bold()
+                    .padding()
+                
+                Spacer()
+            }
+            
+            Text("Fahrenheit")
+                .font(.title2)
+                .bold()
+            
+            HStack {
+                Spacer()
+                Text("\( String(format: "%.0f", fahrenheit)) F°")
+                    .font(.title2)
+                    .bold()
+                    .padding()
+                
+                
+                Spacer()
+            }
+            Spacer()
+        }
+        .padding(.horizontal)
+        .navigationTitle("Temperature Converter")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        NavigationView {
         ContentView()
+        }
     }
 }
